f6676a1b4f9fda146b4f094b73cefafd6ea532f5
viethahahff <12345vietkp@gmail.com>
2024-12-21 09:18:55 +0700

    thongtincanhan

---
+++ NhomVTHCK/app/src/main/java/com/example/nhomvthck/MainActivityThongTinCaNhan.java
@@ -0,0 +1,13 @@
+package com.example.nhomvthck;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivityThongTinCaNhan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
